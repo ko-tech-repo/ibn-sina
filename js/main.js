052fb0c17c87518a0a1ba246fa9a5e4cf092ca9b
hichamoujari <hichamoujari99@gmail.com>
2021-10-02 20:51:05 +0200

    by hicham

--- js/main.js
+++ js/main.js
@@ -7,18 +7,18 @@ setTimeout(() => {
 //awardsCounter
 //teachersCounter
 //studentsCounter
-/*
-const setCounter = (id, current, max, vitesse, aug) => {
+
+const setCounter = (id, current, max, aug) => {
     var ele = document.getElementById(id);
     ele.innerHTML = (current + aug) + "+";
     if (current + aug < max) setTimeout(() => {
-        setCounter(id, current + aug, max, vitesse, aug)
-    }, vitesse)
+        setCounter(id, current + aug, max, aug)
+    }, 100)
     else {
         ele.innerHTML = max + "+";
     }
 }
-setCounter("laureatsCounter", 0, 4999, 1, 20);
-setCounter("awardsCounter", 0, 25, 40, 1);
-setCounter("studentsCounter", 0, 5000, 1, 20);
-setCounter("teachersCounter", 0, 100, 10, 1);*/
+setCounter("laureatsCounter", 0, 4999, 100);
+setCounter("awardsCounter", 0, 25, 1);
+setCounter("studentsCounter", 0, 5000, 100);
+setCounter("teachersCounter", 0, 200, 5);
